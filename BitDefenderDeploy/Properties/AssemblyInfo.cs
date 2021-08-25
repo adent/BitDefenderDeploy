51f817e97bc4fb0121311387a3cee8318cb02953
Andrew Dent <andrew@dentaur.com>
2021-08-25 23:09:50 +1000

    Changes to the assembly info file

--- BitDefenderDeploy/Properties/AssemblyInfo.cs
+++ BitDefenderDeploy/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BitDefenderDeploy")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Utility for MSPs to deploy Bit Defender")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Dentaur Pty Ltd")]
 [assembly: AssemblyProduct("BitDefenderDeploy")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Dentaur Pty Ltd - Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
